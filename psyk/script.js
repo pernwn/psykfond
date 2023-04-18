6fbbd16893977d95c1a6ebab1c0e374e829ca522
mikael1103 <momexgaming@gmail.com>
2023-04-18 12:09:31 +0200

    fade in effekt

--- psyk/script.js
+++ psyk/script.js
@@ -4,3 +4,23 @@ function minFunktion() {
 }
 
 minFunktion(); ––– Husk at kalde funktionen for den skal vise beskeden */
+
+/*Fade in effekt, genbruges når man skriver reveal efter tag
+https://alvarotrigo.com/blog/css-animations-scroll/*/
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
