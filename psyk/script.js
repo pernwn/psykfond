38d0519f1705d007756d7070a6d335c069f22c59
pernwn <pernillepjohansen@hotmail.com>
2023-04-20 11:19:30 +0200

    kenderdu

--- psyk/script.js
+++ psyk/script.js
@@ -1,4 +1,68 @@
+//Variables:
 
+const aLeft = document.getElementById("arrowleft");
+const aRight = document.getElementById("arrowright");
+const kender = document.getElementById("kender");
+const stjerne = document.getElementById("stjerne");
+
+//Array:
+const carousel = [kender, stjerne];
+
+carousel[0].style.display = "flex";
+carousel[1].style.display = "none";
+
+
+
+//knapper:
+aRight.addEventListener("click", nextPic);
+aLeft.addEventListener("click", previousPic);
+
+
+//Funktioner:
+function nextPic(){
+    carousel[0].style.display = "none"; // Skjul den viste
+    carousel.push(carousel[0]); // Put den første ind til sidst
+    carousel.shift(); // Fjern den første og flyt alle en ned
+    carousel[0].style.display = "flex"; // Vis den der nu er først
+}
+
+function previousPic(){
+    carousel[0].style.display = "none";
+    carousel.unshift(carousel[carousel.length-1]);
+    carousel.pop();
+    carousel[0].style.display = "flex";
+}
+
+
+
+/*FORSØG MED BURGER SLIDE IN*/
+aRight.addEventListener("click", slide)
+let visible = false;
+function slide(){
+    if(visible == false){
+        stjerne.style.animationName = "slideIn";
+        kender.style.animationName = "slideOut";
+        visible = true;
+    }else{
+        kender.style.animationName = "slideOut";
+        stjerne.style.animationName = "slideIn";
+        visible = false;
+    }
+}
+
+aLeft.addEventListener("click", slide)
+
+function slide(){
+    if(visible == false){
+        kender.style.animationName = "slideOut";
+        stjerne.style.animationName = "slideIn";
+        visible = true;
+    }else{
+        stjerne.style.animationName = "slideIn";
+        kender.style.animationName = "slideOut";
+        visible = false;
+    }
+}
 
 /*Fade in effekt, genbruges når man skriver reveal efter tag
 https://alvarotrigo.com/blog/css-animations-scroll/*/
@@ -19,4 +83,6 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+
 
